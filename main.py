e9457ae2f405b89f95bff54690b7ca84ae5b750d
fabianwinter93 <fabianwinter93@gmail.com>
2024-08-13 12:02:54 +0200

    group importance added

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ import os
 from scipy.stats.qmc import Sobol, discrepancy
 from scipy.stats import norm, truncnorm
 
+import matplotlib.pyplot as plt
+
 #REPO_ID = "mayflowergmbh/Wiedervereinigung-7b-dpo-laser"
-#REPO_ID = "VAGOsolutions/SauerkrautLM-1.5b"
+REPO_ID = "VAGOsolutions/SauerkrautLM-1.5b"
 #REPO_ID = "VAGOsolutions/SauerkrautLM-gemma-2-2b-it"
-REPO_ID = "HuggingFaceTB/SmolLM-135M"
+#REPO_ID = "HuggingFaceTB/SmolLM-135M"
 
 
 config = AutoConfig.from_pretrained(REPO_ID)
@@ -77,7 +79,6 @@ def get_save_inputs_hook(name):
     return save_inputs_hook
 
 
-
 with torch.no_grad():
     for name, mdl in model.named_modules():
         mlp_or_attn = ("self_attn" in name or "mlp" in name) and "_proj" in name
@@ -113,14 +114,14 @@ def perturbe(model, eps, scale, seed):
                 #    if mdl.bias is None:
                 #        mdl.bias = torch.nn.Parameter(torch.zeros((mdl.out_features)))
             
-                seed_i = seed_gen.integers(1000, 100000)
+                seed_i = seed_gen.integers(10, 1000000)
                 noise = get_noise(mdl.weight.shape, seed_i)
 
                 noise = torch.Tensor(noise).to(mdl.weight.device) * eps * scale
                 #print(np.linalg.norm(mdl.weight), mdl.weight.mean(), mdl.weight.std())
 
-                #mdl.weight += noise 
-                torch.add(mdl.weight, noise, out=mdl.weight)
+                mdl.weight += noise 
+                #torch.add(mdl.weight, noise, out=mdl.weight)
 
 def update(model, grad, lr, seed):
 
@@ -135,7 +136,7 @@ def update(model, grad, lr, seed):
             
             #if ((is_weight := hasattr(mdl, "weight")) or (is_bias := hasattr(mdl, "bias"))) and not emb_or_head:                
             if (is_weight := hasattr(mdl, "weight")) and not emb_or_head and mlp_or_attn:                
-                seed_i = seed_gen.integers(1000, 100000)
+                seed_i = seed_gen.integers(10, 1000000)
                 noise = get_noise(mdl.weight.shape, seed_i)
                 noise = torch.Tensor(noise).to(mdl.weight.device)
 
@@ -172,10 +173,10 @@ def update(model, grad, lr, seed):
                 #print(name, torch.linalg.norm(update), torch.linalg.norm(momentum_dict[name]), momentum_dict[name].mean(), momentum_dict[name].std())
                 #print(update.mean(), update.std())
                 #update = update / (1e-5 + update.std())
-                #mdl.weight -= lr * update# + mdl.weight * 0.01)
+                mdl.weight -= lr * update# + mdl.weight * 0.01)
                 #mdl.weight -= learning_rates[name] * (update)# + mdl.weight * 0.01)
                 #mdl.weight -= lr * (momentum_dict[name])# + mdl.weight * 0.1)
-                torch.add(mdl.weight, -lr*update, out=mdl.weight)
+                #torch.add(mdl.weight, -lr*update, out=mdl.weight)
 
 def get_weight_scores(model, grad, seed):
     
@@ -191,13 +192,13 @@ def get_weight_scores(model, grad, seed):
 
             #if ((is_weight := hasattr(mdl, "weight")) or (is_bias := hasattr(mdl, "bias"))) and not emb_or_head:                
             if (is_weight := hasattr(mdl, "weight")) and not emb_or_head and mlp_or_attn:                
-                seed_i = seed_gen.integers(1000, 100000)
+                seed_i = seed_gen.integers(10, 1000000)
                 noise = get_noise(mdl.weight.shape, seed_i)
                 noise = torch.Tensor(noise).to(mdl.weight.device)
 
-                X = stored_inputs[name]
-                W = mdl.weight
-                g = grad * noise
+                X = stored_inputs[name].to(mdl.weight.device)
+                W = mdl.weight.to(mdl.weight.device)
+                g = (grad * noise).to(mdl.weight.device)
 
                 #print(name, X.shape, W.shape, g.shape)
                 s = torch.einsum("bti,di,di->btdi", X, W, g).mean([0,1])
@@ -216,8 +217,11 @@ def reset_noise(model, eps, seed):
     perturbe(model, eps, 1, seed)
 
 
-SEED = 1000
-EPS = 1e-2
+B = 1
+S = 64
+
+SEED = 0
+EPS = 1e-1 * (1/(B**0.5))
 LR = 1e-2
 
 dataset = load_dataset("wikimedia/wikipedia", "20231101.de", streaming=True)["train"]
@@ -242,7 +246,7 @@ def batchgen(B, S):
             curr = curr[B:, :]
             
 
-bg = batchgen(4, 64+1)
+bg = batchgen(B, S+1)
 
 
 with torch.no_grad():
@@ -255,8 +259,8 @@ with torch.no_grad():
         SEED = SEED + 1
         batch = next(bg)
         
-        inputs = batch[..., :-1].to(model.model.embed_tokens.weight.device)
-        target = torch.nn.functional.one_hot(batch[..., 1:], config.vocab_size).type(torch.float32).to("cpu")
+        inputs = batch[:, :-1].to(model.model.embed_tokens.weight.device)
+        target = torch.nn.functional.one_hot(batch[:, 1:], config.vocab_size).type(torch.float32).to("cpu")
 
 
 
@@ -283,47 +287,97 @@ with torch.no_grad():
 
         reset_noise(model, EPS, SEED)
 
+
+
         
         stored_inputs_bool = True
         y0 = model.forward(inputs)["logits"].to("cpu")
+        l0 = torch.nn.functional.cross_entropy(y0, target)
+        print(l0)
         stored_inputs_bool = False
 
         weight_scores = get_weight_scores(model, projected_grad, SEED)
 
 
-        attn_group = {}
-        mlp_group = {}
+        mlp_groups = {}
+        kv_groups = {}
+        qo_groups = {}
 
-        for name, mdl in model.named_modules():
-            mlp_or_attn = ("self_attn" in name or "mlp" in name) and "_proj" in name
-            emb_or_head = "embed_tokens" in name or "lm_head" in name
-            
-            
-            #if ((is_weight := hasattr(mdl, "weight")) or (is_bias := hasattr(mdl, "bias"))) and not emb_or_head:                
-            if (is_weight := hasattr(mdl, "weight")) and not emb_or_head and mlp_or_attn:                
-
-                score = weight_scores[name]
-                _, _, lid, ltype, proj = name.split(".")
-                lid = int(lid)
-                if ltype == "mlp":
-                    if lid not in mlp_group:
-                        mlp_group[lid] = 0
-
-                    if score > mlp_group[lid]:
-                        mlp_group[lid] = score
-
-
-                elif ltype == "attn":
-                    if lid not in attn_group:
-                        attn_group[lid] = 0
-                    
-                    
-                    if score > attn_group[lid]:
-                        attn_group[lid] = score
-        
-        print(mlp_group)
-        print(attn_group)
                 
+        for lid in range(len(model.model.layers)):
+            layer = model.model.layers[lid]
+
+            u = layer.mlp.up_proj.weight
+            g = layer.mlp.gate_proj.weight
+            d = layer.mlp.down_proj.weight
+            
+            u_score = weight_scores[f"model.layers.{lid}.mlp.up_proj"]
+            g_score = weight_scores[f"model.layers.{lid}.mlp.gate_proj"]
+            d_score = weight_scores[f"model.layers.{lid}.mlp.down_proj"]
+
+            k = layer.self_attn.k_proj.weight
+            v = layer.self_attn.v_proj.weight
+            
+            k_score = weight_scores[f"model.layers.{lid}.self_attn.k_proj"]
+            v_score = weight_scores[f"model.layers.{lid}.self_attn.v_proj"]
+            
+            q = layer.self_attn.q_proj.weight
+            o = layer.self_attn.o_proj.weight
+
+            q_score = weight_scores[f"model.layers.{lid}.self_attn.q_proj"]
+            o_score = weight_scores[f"model.layers.{lid}.self_attn.o_proj"]
+
+
+            ugd_group = torch.Tensor([max([u_score[i, :].sum(), g_score[i, :].sum(), d_score[:, i].sum()]) for i in range(u.shape[0])])
+            kv_group = torch.Tensor([max([k_score[i, :].sum(), v_score[i, :].sum()]) for i in range(k.shape[0])])
+            qo_group = torch.Tensor([max([q_score[i, :].sum(), o_score[:, i].sum()]) for i in range(q.shape[0])])
+
+
+            
+                
+            cutoff_idx = torch.argsort(ugd_group, descending=True)[(ugd_group.shape[0]//100)*90]
+
+            keep = []
+            for iii in range(ugd_group.shape[0]):
+                if ugd_group[iii] > ugd_group[cutoff_idx]:
+                    keep.append(iii)
+            keep = torch.IntTensor(keep)
+
+
+
+            layer.mlp.up_proj.weight = torch.nn.Parameter(u[keep, :])
+            layer.mlp.up_proj.in_features = u.shape[-1]
+            layer.mlp.up_proj.out_features = keep.shape[-1]
+            
+            layer.mlp.gate_proj.weight = torch.nn.Parameter(g[keep, :])
+            layer.mlp.gate_proj.in_features = g.shape[-1]
+            layer.mlp.gate_proj.out_features = keep.shape[-1]
+            
+            layer.mlp.down_proj.weight = torch.nn.Parameter(d[:, keep])
+            layer.mlp.down_proj.in_features = keep.shape[-1]
+            layer.mlp.down_proj.out_features = d.shape[-1]
+            
+            #sorted_idx = torch.argsort(kv_group, descending=True)
+            #keep = kv_group > kv_group[int(kv_group.shape[0]* 0.8)]
+
+            #layer.self_attn.k_proj.weight = torch.nn.Parameter(k[keep, :])
+            #layer.self_attn.k_proj.weight = torch.nn.Parameter(v[keep, :])
+
+            #sorted_idx = torch.argsort(qo_group, descending=True)
+            #keep = qo_group > qo_group[int(qo_group.shape[0]* 0.8)]
+            
+            #print(o)
+            #layer.self_attn.q_proj.weight = torch.nn.Parameter(q[keep, :])
+            #layer.self_attn.o_proj.weight = torch.nn.Parameter(o[:, keep])
+            #print(layer.self_attn.o_proj.weight)
+
+
+
+
+        y0 = model.forward(inputs)["logits"].to("cpu")
+        l0 = torch.nn.functional.cross_entropy(y0, target)
+        print(l0)
+        exit()        
         #update(model, projected_grad, LR, SEED)
 
 
